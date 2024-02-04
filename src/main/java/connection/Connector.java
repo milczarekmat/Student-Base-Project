1859e0c61d57118f0ae652ee02ce596b9307a65e
Lecrut <filip.lecrut12@gmail.com>
2024-02-04 20:40:57 +0100

    utworzenie klasy Connector

---
+++ src/main/java/connection/Connector.java
@@ -0,0 +1,16 @@
+package connection;
+
+public class Connector {
+
+    private boolean loading;
+
+    private boolean connected;
+
+    public static void connect() {
+        while (true) {
+            System.out.print("1");
+        }
+
+    }
+
+}
