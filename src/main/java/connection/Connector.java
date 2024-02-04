b795fddcfb09c3453ef928d876594abddffb5189
Lecrut <filip.lecrut12@gmail.com>
2024-02-04 21:10:51 +0100

    utworzenie połączenia

--- src/main/java/connection/Connector.java
+++ src/main/java/connection/Connector.java
@@ -1,16 +1,36 @@
 package connection;
 
+import java.io.*;
+import java.net.*;
 public class Connector {
 
-    private boolean loading;
+    private static boolean loading;
 
-    private boolean connected;
+    private static boolean connected;
+
+    private static Socket socket = null;
+    private static DataInputStream input = null;
+    private DataOutputStream out = null;
 
     public static void connect() {
-        while (true) {
-            System.out.print("1");
+        try {
+            loading = true;
+            connected = false;
+            socket = new Socket("localhost", 8080);
+            System.out.println("Connected");
         }
-
+        catch (UnknownHostException u) {
+            System.out.println(u);
+            loading = false;
+            return;
+        }
+        catch (IOException i) {
+            System.out.println(i);
+            loading = false;
+            return;
+        }
+        loading = false;
+        connected = true;
     }
 
 }
