b299ad29d2b38b4909fe1a0663926b2b5c09b7b9
Lecrut <filip.lecrut12@gmail.com>
2024-02-05 16:32:43 +0100

    przetrzymanie studentow

--- src/main/java/client/controllers/StudentListController.java
+++ src/main/java/client/controllers/StudentListController.java
@@ -1,7 +1,16 @@
 package client.controllers;
 
+import db.entities.Student;
 import javafx.scene.control.MenuBar;
+
+import java.util.ArrayList;
 
 public class StudentListController {
     public MenuBar menu;
+
+    public ArrayList<Student> students = new ArrayList<>();
+
+    public void setStudents(ArrayList<Student> s) {
+        students = s;
+    }
 }
