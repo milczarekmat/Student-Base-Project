582b9470577f9608683df1bd6c9913d7815d81bd
Lecrut <filip.lecrut12@gmail.com>
2024-02-06 22:43:09 +0100

    znalezienie studenta

--- src/main/java/client/controllers/MainPageController.java
+++ src/main/java/client/controllers/MainPageController.java
@@ -1,7 +1,6 @@
 package client.controllers;
 
 import client.connection.Connector;
-import db.entities.Student;
 import db.entities.StudentGrade;
 import db.helperClasses.SubjectMeanInfo;
 import javafx.collections.FXCollections;
@@ -9,12 +8,8 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-
-
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -28,22 +23,33 @@ public class MainPageController implements Controller, Initializable {
     @FXML
     public TextField findStudent;
     @FXML
-    private ListView<String> listView;
+    public TableView<SubjectMeanInfo> tableView;
 
-    private List<String> grades;
+    @FXML
+    public TableColumn<SubjectMeanInfo, String> name;
+
+    @FXML
+    public TableColumn<SubjectMeanInfo, Float> grade;
+
+    private List<SubjectMeanInfo> grades;
 
     @FXML
     private void handleSearchButton(ActionEvent event) {
         Integer studID = Integer.parseInt(findStudent.getText());
         Set<StudentGrade> s = Connector.searchStudentById(studID);
 
-        grades = s.stream().map(d -> new SubjectMeanInfo(d.getSubject().getName(), d.getGrade().getValue()).toString()).collect(Collectors.toList());
-        grades.forEach(System.out::println);
-        listView.setItems(FXCollections.observableList(grades));
+        grades = s.stream().map(d -> new SubjectMeanInfo(d.getSubject().getName(), d.getGrade().getValue())).collect(Collectors.toList());
+
+        if (grades.size() != 0 && tableView != null) {
+            ObservableList<SubjectMeanInfo> listaStudentow = FXCollections.observableArrayList(grades);
+            name.setCellValueFactory(new PropertyValueFactory<>("subjectName"));
+            grade.setCellValueFactory(new PropertyValueFactory<>("mean"));
+            tableView.setItems(listaStudentow);
+        }
     }
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        listView = new ListView<>();
-    }
+    public void initialize(URL url, ResourceBundle resourceBundle) {}
+
 }
+
